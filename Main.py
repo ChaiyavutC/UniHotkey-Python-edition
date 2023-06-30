6535482f4e26a0189b2880398c072ec4d60265df
Iz <134151822+ChaiyavutC@users.noreply.github.com>
2023-07-01 00:54:04 +0700

    v.0.0.0.3beta
    - system_move_active_win_along_mouse
    - system_move_active_win_to_next_monitor_or_move_to_nearest_area : nearest monitor
    - hotkey_system_vol_down
    - hotkey_system_media_next
    - hotkey_system_media_pause
    - hotkey_system_media_stop
    - hotkey_system_media_previous

--- Main.py
+++ Main.py
@@ -1,53 +1,121 @@
 ##Hotkey
-from ahk import AHK
-
+import keyboard as key
 import win32gui
 import win32con
 
+import pyautogui
+
 import myfunction
+
+import tkinter as tk
 
 target_mic_name = "Microphone (Razer Seiren Mini)"
 volume = myfunction.system_mic_set_target(target_mic_name)
 active_mic_mute = myfunction.system_mic_toggle_mute(0, volume)
 
-ahk = AHK()
-
 def hotkey_system_mic_toggle_mute():
     global active_mic_mute
     active_mic_mute = myfunction.system_mic_toggle_mute(active_mic_mute, volume)
+    if active_mic_mute == 1:
+        mic_state.config(text = 'Muted')
+    else:
+        mic_state.config(text = 'Unmuted')
     return
 
-def hotkey_system_minimize_active_win():
+def hotkey_system_move_active_win_to_next_monitor_or_move_to_nearest_area():
+    ActiveWin = win32gui.GetForegroundWindow()
+    myfunction.system_move_active_win_to_next_monitor_or_move_to_nearest_area(ActiveWin)
+    return
+
+def hotkey_minimize_active_win():
+    print("System.win manager : Minimize active window")
     ActiveWin = win32gui.GetForegroundWindow()
     win32gui.ShowWindow(ActiveWin, win32con.SW_MINIMIZE)
-    print("System.win manager : Minimize active window")
     return
 
-def hotkey_system_move_active_win_to_next_monitor():
+def hotkey_move_active_win_along_mouse():
+    print("System.win manager : Move active window along mouse")
     ActiveWin = win32gui.GetForegroundWindow()
-    myfunction.system_move_active_win_to_next_monitor(ActiveWin)
-    print("System.win manager : Move active window to next monitor")
+    myfunction.system_move_active_win_along_mouse(ActiveWin)
+    return
+
+def hotkey_move_active_win_backward():
+    print("System.win manager : Move active window to back")
+    ActiveWin = win32gui.GetForegroundWindow()
+    win32gui.SetWindowPos(ActiveWin, win32con.HWND_BOTTOM, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+    return
+
+def hotkey_toggle_on_top_active_window():
+    print("System.win manager : Toggle On Top active window")
+    ActiveWin = win32gui.GetForegroundWindow()
+    window_styles = win32gui.GetWindowLong(ActiveWin, win32con.GWL_EXSTYLE)
+    is_on_top = window_styles & win32con.WS_EX_TOPMOST
+
+    # Toggle the window on top
+    if is_on_top:
+        win32gui.SetWindowPos(ActiveWin, win32con.HWND_NOTOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+    else:
+        win32gui.SetWindowPos(ActiveWin, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
     return
 
 def hotkey_system_vol_up():
-    ahk.key_press("VOLUME_UP")
+    pyautogui.press('volumeup')
     print("System.vol : Increased vol")
     return
-    
+
 def hotkey_system_vol_down():
-    ahk.key_press("VOLUME_Down")
-    print("System.vol : Decreased down")
+    pyautogui.press('volumedown')
+    print("System.vol : Decreased vol")
     return
-    
+
+def hotkey_system_media_next():
+    pyautogui.press('nexttrack')
+    print("System.media : Next")
+    return
+
+def hotkey_system_media_pause():
+    pyautogui.press('playpause')
+    print("System.media : Next")
+    return
+
+def hotkey_system_media_stop():
+    pyautogui.press('stop')
+    print("System.media : Next")
+    return
+
+def hotkey_system_media_previous():
+    pyautogui.press('prevtrack')
+    print("System.media : Previous")
+    return
+
 #system_mic_toggle_mute
-ahk.add_hotkey("alt+shift+z", callback=hotkey_system_mic_toggle_mute)
+key.add_hotkey("alt+shift+z",hotkey_system_mic_toggle_mute)
 #system_move_active_win_to_other_monitors
-ahk.add_hotkey("alt+shift+r", callback=hotkey_system_move_active_win_to_next_monitor)
+key.add_hotkey("alt+shift+r",hotkey_system_move_active_win_to_next_monitor_or_move_to_nearest_area)
 #system_minimum_active_win
-ahk.add_hotkey("alt+shift+e", callback=hotkey_system_minimize_active_win)
+key.add_hotkey("alt+shift+e",hotkey_minimize_active_win)
+#system_move_active_win_along_mouse
+key.add_hotkey("alt+shift+q",hotkey_move_active_win_along_mouse)
+#system_move_active_win_backward
+key.add_hotkey("alt+shift+b",hotkey_move_active_win_backward)
+#system_toggle_on_top_active_window
+key.add_hotkey("alt+shift+a",hotkey_toggle_on_top_active_window)
 #system_vol_up
-ahk.add_hotkey("alt+shift+up", callback=hotkey_system_vol_up)
+key.add_hotkey("ctrl+F2",hotkey_system_vol_up)
 #system_vol_down
-ahk.add_hotkey("alt+shift+down", callback=hotkey_system_vol_down)
-ahk.start_hotkeys()  # start the hotkey process thread
-ahk.block_forever()  # not strictly needed in all scripts -- stops the script from exiting; sleep forever
+key.add_hotkey("ctrl+F1",hotkey_system_vol_down)
+#system_media_next
+key.add_hotkey("ctrl+F5",hotkey_system_media_next)
+#system_media_pause
+key.add_hotkey("ctrl+F6",hotkey_system_media_pause)
+#system_media_stop
+key.add_hotkey("ctrl+F7",hotkey_system_media_stop)
+#system_media_previous
+key.add_hotkey("ctrl+F8",hotkey_system_media_previous)
+#key.wait()
+
+mainUI = tk.Tk()
+mainUI.title('Universal v.0.0.0.3beta by ChaiyavutC')
+mic_state = tk.Label(mainUI, text='Muted')
+mic_state.pack()
+mainUI.mainloop()
--- myfunction.py
+++ myfunction.py
@@ -10,6 +10,10 @@ import win32api
 import win32con
 import time
 
+import pyautogui
+
+import keyboard as key
+
 """
 This section is for Toggle Mute selected microphone result to whole system.
 """
@@ -65,11 +69,11 @@ def system_mic_toggle_mute(active_mic_mute, volume):
             print("System.mic : Muted and Checked already muted")
     return active_mic_mute
 
-"""
-This section is for move the active windows to next monitor.
-"""
-def system_move_active_win_to_next_monitor(window_handle):
-        # Check if the window is maximized
+def system_move_active_win_to_next_monitor_or_move_to_nearest_area(window_handle):
+    """
+    This section is for move the active windows to next monitor.
+    """
+    # Check if the window is maximized
     window_placement = win32gui.GetWindowPlacement(window_handle)
     #; window_placement[1] refers to the showCmd field of the window_placement tuple. This field represents the current show command for the window, indicating its state (such as whether it is maximized, minimized, or in a normal state).
     if window_placement[1] == win32con.SW_MAXIMIZE:
@@ -81,31 +85,75 @@ def system_move_active_win_to_next_monitor(window_handle):
     # Get number of monitors
     num_monitors = len(monitor_info)
 
-    # Find the current monitor index based on window position
+    # Reset
     current_monitor_index = None
     #; win32gui.GetWindowRect(window_handle) is used to obtain the bounding rectangle of the window specified by the window_handle.
     window_rect = win32gui.GetWindowRect(window_handle)
-    # get width and height from the bounding rectangle of the window.
-    window_width = window_rect[2] - window_rect[0]
-    window_height = window_rect[3] - window_rect[1]
-    # get x, y of the window (x determine from top left corner of the screen to window_rect[0]) (x determine from top left corner of the screen to window_rect[1])
-    window_x = window_rect[0] + window_width // 2
-    window_y = window_rect[1] + window_height // 2
+    # get center x, y of the window use to define what monitor of window at (x determine from top left corner of the screen to window_rect[0]) (x determine from top left corner of the screen to window_rect[1])
+    window_center_x = (window_rect[2] + window_rect[0]) / 2
+    window_center_y = (window_rect[3] + window_rect[1]) / 2
     
+    # Find the current monitor index based on window position
     for i, monitor in enumerate(monitor_info):
         #; monitor[2] refers to the third element of the monitor tuple, which contains the bounding rectangle coordinates of the monitor.
         monitor_left = monitor[2][0] #; represents the left coordinate of the monitor's bounding rectangle.
         monitor_top = monitor[2][1] #; represents the top coordinate of the monitor's bounding rectangle.
         monitor_right = monitor[2][2] #; represents the right coordinate of the monitor's bounding rectangle.
         monitor_bottom = monitor[2][3] #; represents the bottom coordinate of the monitor's bounding rectangle.
+        
         #is used to check if the window's position falls within the bounding rectangle of a particular monitor. To simply, to get index of monitor which active window at.
-        if window_x >= monitor_left and window_x < monitor_right and window_y >= monitor_top and window_y < monitor_bottom:
+        if window_center_x >= monitor_left and window_center_x < monitor_right and window_center_y >= monitor_top and window_center_y < monitor_bottom:
             current_monitor_index = i
             break
 
     # Increment monitor_index to cycle to the next monitor
     if current_monitor_index is not None:
         monitor_index = (current_monitor_index + 1) % num_monitors #; '% num_monitor' uses to cycle index of monitor in number of monitors.
+        # Get coordinate of window of screen
+        window_x_of_screen = window_rect[0] - monitor_left
+        window_y_of_screen = window_rect[1] - monitor_top
+    # The active window will be moved to the nearest monitor if it is not within visible area.
+    elif current_monitor_index is None:
+        
+        window_width = window_rect[2] - window_rect[0]
+        window_height = window_rect[3] - window_rect[1]
+        
+        # Iterate through each monitor and find the nearest monitor
+        nearest_monitor = None
+        nearest_distance = float('inf')
+        
+        for monitor in monitor_info:
+            #; monitor[2] refers to the third element of the monitor tuple, which contains the bounding rectangle coordinates of the monitor.
+            monitor_rect = monitor[2]
+
+            # Calculate the distance between the center of the active window and the monitor
+            monitor_center_x = (monitor_rect[2] + monitor_rect[0]) / 2
+            monitor_center_y = (monitor_rect[3] + monitor_rect[1]) / 2
+            window_center_x = (window_rect[2] + window_rect[0]) / 2
+            window_center_y = (window_rect[3] + window_rect[1]) / 2
+            distance = ((window_center_x - monitor_center_x) ** 2 + (window_center_y - monitor_center_y) ** 2) ** 0.5
+
+            # Update nearest monitor if the distance is smaller
+            if distance < nearest_distance:
+                nearest_monitor = monitor_rect
+                nearest_distance = distance
+
+        if nearest_monitor is not None:
+            # Calculate the position to move the active window
+            target_x = max(nearest_monitor[0], min(nearest_monitor[2] - window_width, window_center_x - window_width / 2))
+            target_y = max(nearest_monitor[1], min(nearest_monitor[3] - window_height, window_center_y - window_height / 2))
+            """
+            Thai comment จาก "target_x = max(nearest_monitor[0], min(nearest_monitor[2] - window_width, window_center_x - window_width / 2))"
+            - max(nearest_monitor[0], ?) : ใช้เพื่อมั่นใจว่า window จะไม่ตกขอบซ้ายของจอ
+            - min(nearest_monitor[2] - window_width, window_center_x - window_width / 2)
+                - ส่วน nearest_monitor[2] - window_width จะถูกเลือกก็ต่อเมื่อ window อยู่ด้านขวาของขอบจอที่ใกล้ที่สุด
+                - ส่วน window_center_x - window_width / 2 จะถูกเลือกก็ต่อเมื่อ window อยู่ด้านซ้ายของขอบจอที่ใกล้ที่สุด
+            """
+
+            # Move the active window to the nearest monitor area
+            win32gui.SetWindowPos(window_handle, win32con.HWND_TOP, int(target_x), int(target_y), 0, 0, win32con.SWP_NOSIZE)
+            print("System.win manager : Move active window to nearest monitor area")
+        return
 
     # Get the target monitor info
     target_monitor = monitor_info[monitor_index][2]
@@ -114,15 +162,47 @@ def system_move_active_win_to_next_monitor(window_handle):
     target_monitor_right = target_monitor[2]
     target_monitor_bottom = target_monitor[3]
 
-    # Calculate the new position on the target monitor
-    new_x = target_monitor_left + ((target_monitor_right - target_monitor_left) - window_width) // 2
-    new_y = target_monitor_top + ((target_monitor_bottom - target_monitor_top) - window_height) // 2
+    # Calculate the new position on the target monitor and compare ratio of target monitor and current monitor
+    new_x = int(target_monitor_left + window_x_of_screen*((target_monitor_right - target_monitor_left)/(monitor_right - monitor_left)))
+    new_y = int(target_monitor_top + window_y_of_screen*((target_monitor_bottom - target_monitor_top)/(monitor_bottom - monitor_top)))
 
     # Set the new window position
     win32gui.SetWindowPos(window_handle, win32con.HWND_TOP, new_x, new_y, 0, 0, win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
+    print("System.win manager : Move active window to next monitor")
 
     # Maximize the window again if it was originally maximized
     if window_placement[1] == win32con.SW_MAXIMIZE:
-        time.sleep(0.5)
+        time.sleep(0.1)
         win32gui.ShowWindow(window_handle, win32con.SW_MAXIMIZE)
+    return
+
+def system_move_active_win_along_mouse(window_handle):
+    # Check if the window is maximized
+    window_placement = win32gui.GetWindowPlacement(window_handle)
+    #; window_placement[1] refers to the showCmd field of the window_placement tuple. This field represents the current show command for the window, indicating its state (such as whether it is maximized, minimized, or in a normal state).
+    if window_placement[1] == win32con.SW_MAXIMIZE:
+        # Restore the window to its normal state
+        win32gui.ShowWindow(window_handle, win32con.SW_RESTORE)
+    
+    # Get initial mouse position
+    vX1, vY1 = pyautogui.position()
+
+    #; win32gui.GetWindowRect(window_handle) is used to obtain the bounding rectangle of the window specified by the window_handle.
+    window_rect = win32gui.GetWindowRect(window_handle)
+
+    while True:
+        # Get current mouse position
+        vX2, vY2 = pyautogui.position()
+
+        # Calculate new window position
+        new_x = 1.5 * (vX2 - vX1) + window_rect[0]
+        new_y = 1.5 * (vY2 - vY1) + window_rect[1]
+        new_x = int(new_x)
+        new_y = int(new_y)
+
+        # Move the window
+        win32gui.SetWindowPos(window_handle, win32con.HWND_TOP, new_x, new_y,0,0, win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
+        
+        if not (key.is_pressed('q') and key.is_pressed('shift') and key.is_pressed('alt')):
+            break
     return
